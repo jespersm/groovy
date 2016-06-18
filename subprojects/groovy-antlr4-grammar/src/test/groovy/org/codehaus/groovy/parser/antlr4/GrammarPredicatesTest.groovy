f925f0ed7b65e3a6051f702dd7df55bb0911bb6e
Daniel.Sun <realbluesun@hotmail.com>
2016-06-18 18:34:13 +0800

    Fix outdated tests

--- subprojects/groovy-antlr4-grammar/src/test/groovy/org/codehaus/groovy/parser/antlr4/GrammarPredicatesTest.groovy
+++ subprojects/groovy-antlr4-grammar/src/test/groovy/org/codehaus/groovy/parser/antlr4/GrammarPredicatesTest.groovy
@@ -9,27 +9,34 @@ import spock.lang.Unroll
 @Log
 @Unroll
 class GrammarPredicatesTest extends Specification {
+    private static final List<String> BUILT_IN_TYPE_LIST = Arrays.asList("void", "boolean", "byte", "char", "short", "int", "float", "long", "double");
     def "GrammarPredicates: '#name'.isClassName() = #res"(String name, boolean res) {
         setup:
             def tokenStub = new StubFor(TokenStream)
             def array = name.split('\\.')
-            tokenStub.demand.LT { new CommonToken(GroovyParser.IDENTIFIER, array[0]) }
+            tokenStub.demand.LT { new CommonToken(BUILT_IN_TYPE_LIST.contains(array[0]) ? GroovyLangParser.BUILT_IN_TYPE : GroovyLangParser.IDENTIFIER, array[0]) }
             for (int j = 1; j < array.size(); j++) {
                 final i = j
-                tokenStub.demand.LT { new CommonToken(GroovyParser.DOT) }
-                tokenStub.demand.LT { new CommonToken(GroovyParser.IDENTIFIER, array[i]) }
+                tokenStub.demand.LT { new CommonToken(GroovyLangParser.DOT) }
+                tokenStub.demand.LT { new CommonToken(BUILT_IN_TYPE_LIST.contains(array[i]) ? GroovyLangParser.BUILT_IN_TYPE : GroovyLangParser.IDENTIFIER, array[i]) }
             }
-            tokenStub.demand.LT { new CommonToken(GroovyParser.EOF) }
+            tokenStub.demand.LT { new CommonToken(GroovyLangParser.EOF) }
         expect:
             GrammarPredicates.isClassName(tokenStub.proxyDelegateInstance() as TokenStream) == res
         where:
-            name    | res
-            "a"     | false
-            "A"     | true
-             "a.a"   | false // Suppress failing test for now
-            "a.A"   | true
-            "void"  | true
-            "int"   | true
-            "float" | true
+        name      | res
+        "a"       | false
+        "A"       | true
+        "a.a"     | false // Suppress failing test for now
+        "a.A"     | true
+        "void"    | true
+        "boolean" | true
+        "byte"    | true
+        "char"    | true
+        "short"   | true
+        "int"     | true
+        "float"   | true
+        "long"    | true
+        "double"  | true
     }
 }
